76354535604fb407f090e3f863c2aa020fa99b59
shanil <Muhammedshanilizza@gmail.com>
2022-01-22 17:24:12 +0530

    add banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Banner from './Components/Banner/Banner';
 import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Banner />
     </div>
   );
 }
