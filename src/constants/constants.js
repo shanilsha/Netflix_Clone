a0968672735738bda94d3c3ebef600bb8086d722
shanil <Muhammedshanilizza@gmail.com>
2022-01-25 21:13:28 +0530

    change in db

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@
 export const baseUrl = 'https://api.themoviedb.org/3'; 
 
 // APi key
-export const API_KEY ="8103ed6cbf756c41a31211b5245ca813";
+export const API_KEY ="api key";
 
 // image url
  export const imageUrl ='https://image.tmdb.org/t/p/original';
